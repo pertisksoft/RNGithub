a29b101a8584cc5fe9df6c8591adeb33fea74ee6
Nat <dotnetnat@gmail.com>
2018-04-30 21:52:27 +0700

    add package

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {
+  DefaultTheme,
+  Provider as PaperProvider,
+  Button
+} from "react-native-paper";
+import { View, StyleSheet } from "react-native";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#ff5722",
+    accent: "yellow"
+  }
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <Button primary raised onPress={() => console.log("Pressed")}>
+            Press me
+          </Button>
+        </View>
+      </PaperProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
