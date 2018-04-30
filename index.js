28da9b243c0d6072defdf11f16663604d81b046b
Nat <dotnetnat@gmail.com>
2018-04-30 21:42:41 +0700

    add ignore alert

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import _ from "lodash";
+import { YellowBox } from "react-native";
+import "color-term-console";
+import { condensedJsonOutput, setColorTheme } from "color-term-console";
 
-AppRegistry.registerComponent('RNGithub', () => App);
+import App from "./src";
+
+condensedJsonOutput(true);
+
+setColorTheme({
+  log: "orange"
+});
+
+console.disableYellowBox = true;
+YellowBox.ignoreWarnings(["Setting a timer"]);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf("Setting a timer") <= -1) {
+    _console.warn(message);
+  }
+};
+
+AppRegistry.registerComponent("RNGithub", () => App);
